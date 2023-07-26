09142a04fc163e7d43b18758d67da0f96f49ed1d
DanielFernandez <daniele_fernandezb@soy.sena.edu.co>
2023-07-26 01:18:18 -0500

    Ecls

---
+++ Javascript/Ejercicio 2/js/script2.js
@@ -0,0 +1,20 @@
+/*Ejercicio 2 - dado el sueldo de un empleado imprimir en una alerta el 
+nuevo sueldo del empleado si tuvo un aumento del 10%
+
+let sueldo = prompt("Ingrese el valor del suedo")
+let psueldo = 10/100;
+let nsueldo = parseInt(sueldo) + parseInt(sueldo*psueldo)
+
+alert("El nuevo sueldo con el aumento es: $" +nsueldo+ "COP") */
+
+/*Ejercicio 3 - un vendedor que tiene su sueldo base recive una comision del 30% del total de las ventas del mes, el quiere saver cuanto ganara en un mes que tuvo 3 ventas
+let sueldo = prompt("Ingrese el valor del suedo base");
+let venta1 = prompt("Ingrese el monto de la primera venta");
+let venta2 = prompt("Ingrese el monto de la segunda venta");
+let venta3 = prompt("Ingrese el monto de la tercera venta");
+let porcen = 30/100;
+let comision = parseInt(venta1)+ parseInt(venta2)+ parseInt(venta3);
+let resul = parseInt(sueldo) + parseInt(comision*porcen);
+let resulf = parseInt(resul); 
+
+alert("El salario total en el mes es: $" +resulf+ " COP");*/
---
+++ Javascript/Ejercicio 2/js/script3.js
@@ -0,0 +1,15 @@
+/*Ejercicio 1 - Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
+saber cuánto deberá pagar por su compra
+
+let montoTT = prompt("ingrese el monto total de la compra");
+let desc = 25/100;
+let montoPG = parseInt(montoTT) - parseInt(montoTT*desc);
+alert("El monto a pagar con el descuento aplicado es: $"+montoPG+ "COP");*/
+
+/*Ejercicio 2 - Suponga que un individuo desea invertir su capital en un banco y desea saber
+cuánto dinero ganara después de un mes si el banco paga a razón de 2%
+mensual
+let capital = prompt("Ingrese el monto de capital a invertir:");
+let interesM = 2/100;
+let montoF = parseInt(capital*interesM)
+alert("El monto final después de un mes con el interés aplicado es: $" +montoF+ "COP");*/
